4420cdc3ecee13c688205aa457507a3ba095395c
Zachary Kim <zachary.kim@gmail.com>
2013-11-08 17:59:38 -0800

    elpa update

---
+++ elpa/anzu-20131106.1250/anzu-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anzu" "20131106.1250" "Show number of matches in mode-line while searching [github]" 'nil)
---
+++ elpa/helm-20131107.1158/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20131107.1158" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
